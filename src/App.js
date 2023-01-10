67672b1ebb958d69ef51220b0a6b84d56d1e25aa
denashams <dena.shamsizadeh@gmail.com>
2023-01-10 17:41:24 +0330

    added github link

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">weather app</div>;
+  return (
+    <div className="App">
+      <h1>weather app</h1>
+      <footer>
+        {" "}
+        <a
+          href="https://github.com/denashams/my-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source code on github
+        </a>{" "}
+        by Dena Shams
+      </footer>
+    </div>
+  );
 }
 
 export default App;
