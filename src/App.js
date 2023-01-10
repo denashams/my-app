a7bb00ffe94f5f363416f9fc5d7906b6ba6c5b54
denashams <dena.shamsizadeh@gmail.com>
2023-01-10 18:20:03 +0330

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>weather app</h1>
-      <footer>
-        {" "}
-        <a
-          href="https://github.com/denashams/my-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open-source code on github
-        </a>{" "}
-        by Dena Shams
-      </footer>
+      <div className="container">
+        <h1>weather app</h1>
+        <footer>
+          {" "}
+          <a
+            href="https://github.com/denashams/my-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source code on github
+          </a>{" "}
+          by Dena Shams
+        </footer>
+      </div>
     </div>
   );
 }
