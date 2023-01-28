2044c1478d1c04acaaa614e9305940b6f1fd4e82
denashams <dena.shamsizadeh@gmail.com>
2023-01-28 12:51:33 +0330

    added one day forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,31 +1,37 @@
-import React from "react";
-import WeatherIcon from "./WeatherIcon";
+import React, { useState } from "react";
 import "./WeatherForecast.css";
+import WeatherForecastDay from "./WeatherForecastDay";
 import axios from "axios";
 export default function WeatherForecast(props){
+    let [loaded , setLoaded] = useState(false);
+    let [forecast, setForecast]=useState(null); 
     function handleResponse(response) {
-        console.log(response.data);
+    setForecast(response.data.daily);
+    setLoaded(true);
     }
-    let apiKey = "75cct101457333214a7f31a7d08dfbo0";
-    let query = props.city;
-let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${query}&key=${apiKey}&units=metric`;
-axios.get(apiUrl).then(handleResponse);
-    return (
-<div className="WeatherForecast">
-    <div className="row">
-        <div className="col">
-            <div className="forecast-day">
-            Thu
+
+    if (loaded) {
+        console.log(forecast);
+        return (
+            <div className="WeatherForecast">
+                <div className="row">
+                    <div className="col">
+                        
+
+                        <WeatherForecastDay data={forecast[0]}/>
+                    </div>
+                </div>
             </div>
-            <WeatherIcon code="clear-sky-day" size={40}/>
-            <div className="forecast-temperature">
-            <span className="forecast-max-temp">19°</span> <span className="forecast-min-temp">10°</span>
-            </div>
-        </div>
-    </div>
-</div>
+            
+                );
 
-    );
+    } else {
+        let apiKey = "75cct101457333214a7f31a7d08dfbo0";
+        let query = props.city;
+        let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${query}&key=${apiKey}&units=metric`;
+    
+    axios.get(apiUrl).then(handleResponse);
+return null;
 
-
+    }
 }
